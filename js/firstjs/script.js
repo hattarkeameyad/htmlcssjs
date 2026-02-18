8dacb6bf1495dac4c6b5744e83419e00daf8d97e
Mr Ameya Hattarke <ameyahattarke@gmail.com>
2026-02-18 16:34:19 +0530

    flex and js day one

---
+++ js/firstjs/script.js
@@ -0,0 +1,16 @@
+console.log("Hello pratik");
+// this is single line comment
+/* This is multi line comment 
+*/
+  const PI = 3.14;
+function myfunction() {
+    let x = 20;
+    console.log(x);
+
+    alert("HI Ameya");
+    let uname=prompt("Please enter your name");
+    console.log(uname);
+
+    let result= confirm("Yes or No");
+    console.log(result);
+}
