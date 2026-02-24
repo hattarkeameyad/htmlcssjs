47f9f5c282351448cae762521101d2e81725979d
Mr Ameya Hattarke <ameyahattarke@gmail.com>
2026-02-24 16:24:00 +0530

    closure function remaining topic

---
+++ js/function part 2/script.js
@@ -0,0 +1,117 @@
+
+//arrow funcions
+
+let a, b, result;
+function substraction(a, b) {
+
+    console.log(a - b);
+}
+
+let add = (a, b) => console.log(a + b);
+
+let multi = (a, b) => {
+    result = a * b;
+    console.log(result);
+}
+
+substraction(20, 30);
+
+add(40, 50);
+
+multi(10, 5);
+
+//iife function immediately invoked
+(function () {
+    console.log("iife function got called");
+})();
+a = 40; b = 30;
+
+(function () {
+    a = 20, b = 40;
+    console.log("division with iife  " + a / b);
+})();
+
+
+console.log("------------------------------------");
+// Call Back funcion
+
+function aniket(bucket) {
+    console.log("Within the main function...");
+    bucket();
+}
+
+function sayHello() {
+
+    console.log("Call Back function is getting called .....");
+    console.log("Hello");
+}
+
+function saybye() {
+
+    console.log("Call Back function is getting called .....");
+    console.log("bye...........");
+}
+
+aniket(sayHello);
+// aniket(saybye);
+
+
+//example 2
+
+
+function calculate(a, b, operation) {
+    return operation(a, b);
+}
+
+        function add1(x, y) {
+            return x + y;
+        }
+
+        function multiply(x, y) {
+            return x * y;
+        }
+
+        let y=calculate(5, 3, add1);
+console.log("This is log "+calculate(5, 3, add1));       // 8
+console.log("This is variable  "+y);
+console.log(calculate(5, 3, multiply));
+
+
+
+//clousure demo
+
+
+
+function myfun(){
+
+    let c=20;
+    console.log("This is my function ");
+    return c;
+
+}
+
+console.log(myfun());
+
+console.log(myfun());
+
+//new example
+
+function outerFunction() {
+let outerVariable = "I am from outer";
+
+    function innerFunction() {
+        console.log(outerVariable);
+    }
+
+return innerFunction;
+}
+
+const res = outerFunction();
+res();
+res();
+res();
+
+
+
+
+
